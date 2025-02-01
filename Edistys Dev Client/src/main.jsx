8887e87b96fce8bbf2d74e7ac158caba5cd38f69
Muhammad Sabbir Hossain Bin Ahmad <dustobalok@gmail.com>
2025-02-01 15:05:23 +0600

    Packages: tailwind, daisyUI, react router dom have been installed successfully.

--- Edistys Dev Client/src/main.jsx
+++ Edistys Dev Client/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   </StrictMode>,
 )
